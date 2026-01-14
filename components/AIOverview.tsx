c4e6af5dde2ed192fcfc36c03c0ccf3be56695f8
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-01-14 13:59:54 +0500

    Change icon for AI overview to sparkles

--- components/AIOverview.tsx
+++ components/AIOverview.tsx
@@ -40,7 +40,7 @@ export default function AIOverview({ overview, loading, error }: AIOverviewProps
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M13 10V3L4 14h7v7l9-11h-7z"
+                d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
               />
             </svg>
           </div>
